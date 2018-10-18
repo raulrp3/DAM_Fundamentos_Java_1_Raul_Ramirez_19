9e1b0d04292129511928066bc68b3d8e2bf7a176
raulrp3 <raul.ramirez@escuelaestech.es>
2018-10-18 17:47:49 +0200

    Ejercicio 19

---
+++ src/com/company/Main.java
@@ -0,0 +1,77 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static int validarEntero(Scanner sc) {
+        int numeroValidado;
+        while (!sc.hasNextInt()) {
+            System.out.print("¡Introduce un valor numñerico entero! ");
+            sc.next();
+        }
+        numeroValidado = sc.nextInt();
+        return numeroValidado;
+    }
+
+    public static boolean validarNota(int nota) {
+        boolean validado = true;
+        if (nota > 10 || nota < 0) {
+            validado = false;
+        }
+        return validado;
+    }
+
+    public static String daNota(int nota) {
+        String notaString = "";
+        switch (nota) {
+            case 0:
+                notaString = "CERO";
+                break;
+            case 1:
+                notaString = "UNO";
+                break;
+            case 2:
+                notaString = "DOS";
+                break;
+            case 3:
+                notaString = "TRES";
+                break;
+            case 4:
+                notaString = "CUATRO";
+                break;
+            case 5:
+                notaString = "CINCO";
+                break;
+            case 6:
+                notaString = "SEIS";
+                break;
+            case 7:
+                notaString = "SIETE";
+                break;
+            case 8:
+                notaString = "OCHO";
+                break;
+            case 9:
+                notaString = "NUEVE";
+                break;
+            case 10:
+                notaString = "DIEZ";
+                break;
+        }
+        return notaString;
+    }
+
+    public static void main(String[] args) {
+        int nota;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¡Vamos a mostrar una nota numñerica en forma alfabética!");
+        System.out.print("Introduce una nota: ");
+        nota = validarEntero(sc);
+        while (!validarNota(nota)) {
+            System.out.print("¡La nota debe ser entre 0 y 10! ");
+            nota = validarEntero(sc);
+        }
+        sc.close();
+        System.out.printf("La nota %d se escribe: %s\n", nota, daNota(nota));
+    }
+}
